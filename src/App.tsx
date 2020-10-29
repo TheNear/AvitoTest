fa2760ea76dff8945f270b28e9d12bf628522c8b
TheNear <thenearx@gmail.com>
2020-10-29 14:30:36 +0300

    init project

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
 import React from "react";
 import "./App.css";
 
-function App() {
+const App: React.FC = () => {
   return (
     <div className="App">
       <h1>test</h1>
     </div>
   );
-}
+};
 
 export default App;
